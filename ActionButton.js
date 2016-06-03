49374984c92f3a8193f88791e62155f807b0f894
John Murphy <jmichealmurphy@gmail.com>
2016-06-03 16:20:49 -0500

    Update ActionButton.js

--- ActionButton.js
+++ ActionButton.js
@@ -1,4 +1,4 @@
-import React {PropTypes, Component} from 'react';
+import React, {PropTypes, Component} from 'react';
 import {StyleSheet, Text, View, Animated, Easing, TouchableOpacity, PixelRatio } from 'react-native';
 import ActionButtonItem from './ActionButtonItem';
 
